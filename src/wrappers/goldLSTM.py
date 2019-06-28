76a2547ed4166154075d3b3ab21ba20d731f8795
salmashukry <salma.emara@mail.utoronto.ca>
2019-06-28 05:25:33 +0000

    Added LSTM results

--- src/wrappers/goldLSTM.py
+++ src/wrappers/goldLSTM.py
@@ -7,20 +7,23 @@ import context
 
 def main():
 	args = arg_parser.sender_first()
-	cc_repo = path.join(context.third_party_dir, 'gold')
+	cc_repo = path.join(context.third_party_dir, 'goldLSTM')
 	send_src = path.join(cc_repo, 'sender-receiver/sender_receiver/envs', 'example_a2c.py')
 	recv_src = path.join(cc_repo, 'sender-receiver/sender_receiver/envs', 'run_receiver.py')
 	#dependencies = path.join(cc_repo, 'dependencies.sh')
 	if args.option == 'setup':
-		#check_call(dependencies, shell = True)
-                return 
+            #check_call(dependencies, shell = True)
+            return
+
 	if args.option == 'sender':
-		cmd = ['python3' , send_src, args.port]
-		check_call(cmd)
-                return
+            cmd = ['python3' , send_src, args.port]
+            check_call(cmd)
+            return
+
 	if args.option == 'receiver':
-		cmd = [recv_src, args.ip, args.port]
-		check_call(cmd)
-		return
+            cmd = ['python3', recv_src, args.ip, args.port]
+            check_call(cmd)
+            return
+
 if __name__ == "__main__":
 	main()
