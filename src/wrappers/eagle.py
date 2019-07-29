3759e2541fabc1c1b15d7f26284c9af5fff455dd
salmashukry <salma.emara@mail.utoronto.ca>
2019-07-29 01:37:59 +0000

    Added test results for eagle

--- src/wrappers/eagle.py
+++ src/wrappers/eagle.py
@@ -8,8 +8,8 @@ import context
 
 def main():
         args = arg_parser.sender_first()
-        cc_repo = path.join(context.third_party_dir, 'goldIRL')
-        send_src = path.join(cc_repo, 'sender-receiver/sender-receiver/sender_receiver/envs', 'example_a2c.py')
+        cc_repo = path.join(context.third_party_dir, 'eagle')
+        send_src = path.join(cc_repo, 'sender-receiver/sender-receiver/sender_receiver/envs', 'example-xentropy.py')
         recv_src = path.join(cc_repo, 'sender-receiver/sender-receiver/sender_receiver/envs', 'run_receiver.py')
         #dependencies = path.join(cc_repo, 'dependencies.sh')
         if args.option == 'setup':
