e83ff9cba71a9e8b04e9ab13474d1c36f6d29956
Fei Wang <917522022@qq.com>
2020-02-28 22:43:18 -0500

    correct the wrapper file

--- src/wrappers/pythonBBR-plus.py
+++ src/wrappers/pythonBBR-plus.py
@@ -10,17 +10,17 @@ def main():
         args = arg_parser.sender_first()
         cc_repo = path.join(context.third_party_dir, 'eagle-plus')
 	send_src = path.join(cc_repo, 'net-em/net-em/net_em/envs', 'example_xentropy.py')
-        recv_src = path.join(cc_repo, 'net-em/net-em/net_em/envs/connect-Eagle',  'connect')
+        recv_src = path.join(cc_repo, 'net-em/net-em/net_em/envs/connect-Eagle', 'connect')
         if args.option == 'setup':
             return
 
         if args.option == 'sender':
-            cmd = ['python3', python_src, args.port, 'expert', 'pythonBBR']
+            cmd = ['python3', send_src, args.port, 'expert', 'pythonBBR']
             subprocess.check_output(cmd)
             return
 
         if args.option == 'receiver':
-            cmd = [cpp_src, 'receive', args.ip, args.port]
+            cmd = [recv_src, 'receive', args.ip, args.port]
             subprocess.check_output(cmd)
             return
 
